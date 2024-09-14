7fe6b135af690a15906a23a2b53fa70325de2720
Tariq Ayman <tariq.ayman94@gmail.com>
2024-09-14 20:18:12 +0300

    add DTO for auth services

---
+++ src/authentication/dto/profile.dto.ts
@@ -0,0 +1,3 @@
+export class ProfileDto {
+  userId: string;
+}
---
+++ src/authentication/dto/register.dto.ts
@@ -0,0 +1,57 @@
+import { IsString, IsEmail, MinLength, IsNotEmpty, IsEnum, IsDate } from 'class-validator';
+import { UserGender } from 'src/common/enums/UserGender';
+import { UserProvider } from 'src/common/enums/UserProvider';
+import { UserRole } from 'src/common/enums/UserRole';
+import { UserStatus } from 'src/common/enums/UserStatus';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+
+  @IsEnum(UserGender)
+  gender: UserGender;
+
+  @IsDate()
+  DOB: Date;
+
+  @IsEnum(UserProvider)
+  provider: UserProvider;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+
+  @IsEnum(UserStatus)
+  status: UserStatus;
+
+  @IsDate()
+  createdAt: Date;
+
+  @IsDate()
+  updatedAt: Date;
+
+  @IsDate()
+  lastLogin: Date;
+
+  @IsDate()
+  lastLogout: Date;
+}
