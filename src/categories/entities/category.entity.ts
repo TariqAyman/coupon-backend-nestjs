2315c2d748ba0eae8616ed7340844261861e19f8
Tariq Ayman <tariq.ayman94@gmail.com>
2024-09-14 20:18:42 +0300

    add Entity for Category

---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,59 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+  BaseEntity,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Location } from '../../locations/entities/location.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuidv4(); // Generates the UUID in the application
+
+  @Column('json')
+  name!: { en: string; ar: string };
+
+  @Column('json')
+  slug!: { en: string; ar: string };
+
+  @Column('json', { nullable: true })
+  description?: { en: string; ar: string };
+
+  @Column({ nullable: true })
+  image?: string;
+
+  @Column({ nullable: true })
+  icon?: string;
+
+  @Column({ nullable: true })
+  color?: string;
+
+  @ManyToOne(() => User, { nullable: false })
+  createdBy!: User;
+
+  @ManyToOne(() => User, { nullable: true })
+  updatedBy?: User;
+
+  @ManyToMany(() => Location)
+  @JoinTable({
+    name: 'category_locations',
+    joinColumn: { name: 'category_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'location_id', referencedColumnName: 'id' },
+  })
+  location!: Location[];
+
+  @Column({ default: false })
+  isDeleted!: boolean;
+
+  @Column()
+  createdAt!: Date;
+
+  @Column()
+  updatedAt!: Date;
+}
