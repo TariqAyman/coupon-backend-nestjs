1a8e46912b7f09488506e2a3e3b9ad2b81911e79
Tariq Ayman <tariq.ayman94@gmail.com>
2024-09-14 20:20:25 +0300

    init all Modules in projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AdsModule } from './ads/ads.module';
+import { BrandsModule } from './brands/brands.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CouponsModule } from './coupons/coupons.module';
+import { LocationsModule } from './locations/locations.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { DatabaseModule } from './database/database.module';
+import databaseConfig from './database/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60, // Time to live in seconds
+          limit: 10, // Max number of requests in the TTL
+        },
+      ],
+    }),
+    DatabaseModule,
+    AdsModule,
+    UsersModule,
+    BrandsModule,
+    CategoriesModule,
+    CouponsModule,
+    LocationsModule,
+    NotificationsModule,
+    AuthenticationModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
