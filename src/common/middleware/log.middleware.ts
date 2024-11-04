36f7d14922af536f60690f429186c13e815c4de8
Tariq Ayman <tariq.ayman94@gmail.com>
2024-11-04 12:02:34 +0200

    add console.log for request

--- src/common/middleware/log.middleware.ts
+++ src/common/middleware/log.middleware.ts
@@ -4,7 +4,9 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
+    console.log(
+      `Request... ip:${req.ip} method:${req.method} url:${req.url} statusCode: ${res.statusCode} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(req.body)}`,
+    );
     next();
   }
 }
