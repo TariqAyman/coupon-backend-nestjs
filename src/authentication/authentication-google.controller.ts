690946588ac897ff9b14b5e1a2976a0e1627b52b
Tariq Ayman <tariq.ayman94@gmail.com>
2024-12-14 18:52:28 +0200

    fix login by google

--- src/authentication/authentication-google.controller.ts
+++ src/authentication/authentication-google.controller.ts
@@ -28,32 +28,11 @@ export class AuthenticationGoogleController {
   async googleAuthCallback(@Req() req: any, @Res() res: Response) {
     const user = await this.authenticationService.signInByGoogle(req.user);
 
-    console.log(process.env.FRONTEND_DOMAIN_COOKIE);
-
-    res.cookie('token1', user.token, {
-      maxAge: 2592000000,
-      // domain: process.env.FRONTEND_DOMAIN_COOKIE,
-      sameSite: true,
-      secure: false,
-    });
-
-    res.cookie('token2', user.token, {
+    res.cookie('token', user.token.access_token, {
       maxAge: 2592000000,
       domain: process.env.FRONTEND_DOMAIN_COOKIE,
       sameSite: true,
       secure: false,
-    });
-
-    res.cookie('token3', user.token, {
-      maxAge: 2592000000,
-      sameSite: true,
-      secure: true,
-    });
-
-    res.cookie('token4', user.token, {
-      maxAge: 2592000000,
-      sameSite: false,
-      secure: true,
     });
 
     setTimeout(() => {}, 2000);
