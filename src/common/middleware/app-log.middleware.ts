5fb64eb76dc1af34ad0cb745b1e0081ef4d1aa08
Tariq Ayman <tariq.ayman94@gmail.com>
2025-01-01 22:54:02 +0200

    show log only on local or development

--- src/common/middleware/app-log.middleware.ts
+++ src/common/middleware/app-log.middleware.ts
@@ -5,9 +5,11 @@ import { Request, Response, NextFunction } from 'express';
 export class AppLoggerMiddleware implements NestMiddleware {
   private logger = new Logger(`HTTP`);
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(
-      `Logging HTTP request ip:${req.ip} method:${req.method} url:${req.url} statusCode: ${res.statusCode} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(req.body)}`,
-    );
+    if (process.env.NODE_ENV !== 'production') {
+      this.logger.log(
+        `Logging HTTP request ip:${req.ip} method:${req.method} url:${req.url} statusCode: ${res.statusCode} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(req.body)}`,
+      );
+    }
     next();
   }
 }
--- src/common/middleware/log.middleware.ts
+++ src/common/middleware/log.middleware.ts
@@ -4,9 +4,11 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(
-      `Request... ip:${req.ip} method:${req.method} url:${req.url} statusCode: ${res.statusCode} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(req.body)}`,
-    );
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(
+        `Request... ip:${req.ip} method:${req.method} url:${req.url} statusCode: ${res.statusCode} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(req.body)}`,
+      );
+    }
     next();
   }
 }
